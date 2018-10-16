b613a1ab2da8dd0a8d3c81a7409c63baec7d5bde
blacklotus923 <sirius923@yahoo.com>
2018-10-15 20:23:28 -0400

    PEP 8 formatting, cleanliness.

--- SyncTool/synctool.py
+++ SyncTool/synctool.py
@@ -1,107 +1,113 @@
 import os
 import hashlib
 import _pickle as cpickle
-from sys import getsizeof
+import sys
 from platform import system
 
 print('Sup dudes')
 
-########  Globals and user vars  ##########
+# #######  Globals and user vars  ##########
 _configfile = "config.db"
 
-##let's not go off the handle until we're prod ready.  limit 100k results.
+# #let's not go off the handle until we're prod ready.  limit 100k results.
 handbrake = 100000
 
 configuration = {
-    "_cwd" : os.path.abspath(os.getcwd()),
-    "_fname" : os.path.abspath(os.getcwd()),
-    "history" : {}
+    "_cwd": os.path.abspath(os.getcwd()),
+    "_fname": os.path.abspath(os.getcwd()),
+    "history": {}
 }
 
 newfiles = {}
 oldfiles = {}
 
-######## Class Defs ###########
+# ####### Class Defs ###########
 
-#Setting up file class for each file/dir on disk
-#State: 0=no change, 1=updated, 2=new, 3=deleted
+
+# Setting up file class for each file/dir on disk
+# State: 0=no change, 1=updated, 2=new, 3=deleted
 class File:
-    def __init__(self,name,parent,type,size,modified=None,state=2):
-        self.name=name
-        self.parent=parent
-        self.type=type
-        self.size=size
+    def __init__(self, name, parent, objecttype, size, modified=None, state=2):
+        self.name = name
+        self.parent = parent
+        self.objecttype = objecttype
+        self.size = size
         if modified is not None:
-            self.modified=modified
+            self.modified = modified
         if state is not None:
-            self.state=state
+            self.state = state
 
-#No need for this function as we concatinate the full path for dict key later
+# No need for this function as we concatinate the full path for dict key later
 #    def fullpath(self):
 #        return self.parent + '\\' + self.name
 
-######## Generators ###########
+# ####### Generators ###########
 
-#Recursive generator for getting all files and directories under a path
-#Pass the path under which you want to list all files/dirs
-#Returns objects of class File
+
+# Recursive generator for getting all files and directories under a path
+# Pass the path under which you want to list all files/dirs
+# Returns objects of class File
 def files(path):
     if path:
-        for object in os.listdir(path):
-            fullpath = os.path.join(path, object)
+        for p in os.listdir(path):
+            fullpath = os.path.join(path, p)
             if os.path.isdir(fullpath):
-                yield File(object,path,'DIR',os.path.getsize(fullpath),os.path.getmtime(fullpath))
-                #print('DIRECTORY')
+                yield File(p, path, 'DIR', os.path.getsize(fullpath), os.path.getmtime(fullpath))
                 yield from files(fullpath)
             else:
-                yield File(object, path, 'FILE',os.path.getsize(fullpath),os.path.getmtime(fullpath))
+                yield File(p, path, 'FILE', os.path.getsize(fullpath), os.path.getmtime(fullpath))
 
-######## Function Defs ###########
+# ####### Function Defs ###########
 
-##Get human-readable size on disk
+
+# #Get human-readable size on disk
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['','K','M','G','T','P','E','Z']:
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1000.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1000.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
-##Return hash of a filename for saving unique dbs
+
+# #Return hash of a filename for saving unique dbs
 def hashedfilename(filename):
     return str(hashlib.md5(filename.encode()).hexdigest()) + ".db"
 
-##Save dict to disk, True if saved, False if not
-def dictSave(filename, dict):
+
+# #Save dict to disk, True if saved, False if not
+def savedict(filename, dictionary):
     try:
         with open(filename, 'wb') as f:
-            cpickle.dump(dict,f, protocol=3)
+            cpickle.dump(dictionary,f, protocol=3)
         return True
     except IOError as e:
         print("Unable to save " + filename + ": I/O error({0}): {1}".format(e.errno, e.strerror))
         raise e
     except:  # handle other exceptions such as attribute errors
-        print("Unexpected error:", sys.exc_info()[0])
+        print("Unexpected error: ", sys.exc_info()[0])
         raise
 
-##Load dict from disk
-def dictLoad(filename):
+
+# #Load dict from disk
+def loaddict(filename):
     try:
-        with open(filename,'rb') as f:
+        with open(filename, 'rb') as f:
             return cpickle.load(f)
     except IOError as e:
         raise e
     except:  # handle other exceptions such as attribute errors
-        print("Unexpected error:", sys.exc_info()[0])
+        print("Unexpected error: ", sys.exc_info()[0])
         raise
 
-##Compare file dicts for differences
+
+# #Compare file dicts for differences
 def filecompare():
     global oldfiles
     global newfiles
 
     if hashedfilename(configuration["_fname"]) in configuration["history"]:
         try:
-            oldfiles = dictLoad(hashedfilename(configuration["_fname"]))
+            oldfiles = loaddict(hashedfilename(configuration["_fname"]))
         except:
             oldfiles = {}
     else:
@@ -132,76 +138,81 @@ def filecompare():
         print("Since this is the first scan of this directory, make sure to save with savelist.")
         return True
 
-##Set directory to scan
+
+# #Set directory to scan
 def setdir():
     global configuration
     b = True
     while b:
-        dir = input("Enter path to recursively search, c to cancel:  ")
-        if dir == "c":
-            b=False
-        elif os.path.isdir(dir):
-            dir = os.path.abspath(dir)
-            configuration["_cwd"] = dir
-            b=False
+        directory = input("Enter path to recursively search, c to cancel:  ")
+        if directory == "c":
+            b = False
+        elif os.path.isdir(directory):
+            directory = os.path.abspath(directory)
+            configuration["_cwd"] = directory
+            b = False
         else:
-            print(dir + " doesn't appear to be a directory or access is denied.")
+            print(directory + " doesn't appear to be a directory or access is denied.")
     return True
 
-##List past directories
+
+# #List past directories
 def listdirs():
     for k, v in configuration["history"].items():
-        print("%s : ID - %s"%(v,k))
-    _=input("Press any key to continue")
+        print("%s : ID - %s" % (v, k))
+    _ = input("Press any key to continue")
     return True
 
-##Scan selected directory
+
+# #Scan selected directory
 def scandir():
-    ##Init generator
+    # #Init generator
     global configuration
     global newfiles
     if os.path.isdir(configuration["_cwd"]):
         getfiles = files(configuration["_cwd"])
         totalsize = 0
         newfiles.clear()
-        ##Run generator with handbrake, fill dict with full path as key, file objects as values, then delete generator
+        # #Run generator with handbrake, fill dict with full path as key, file objects as values, then delete generator
         try:
             for x in range(0, handbrake):
                 f = (next(getfiles))
                 newfiles[f.parent + '\\' + f.name] = f
-                print (f.parent + '\\' + f.name + ",    " + f.type + ", " + str(f.modified))
+                totalsize += os.path.getsize(f.parent + '\\' + f.name)
+                print(f.parent + '\\' + f.name + ",    " + f.type + ", " + str(f.modified))
+                # #Stats
+            print('\nIterated ' + str(x) + ' times. File dictionary is ' + str(len(newfiles)) + ' in length.')
+            print('\nSize of dict in memory is ' + sizeof_fmt(sys.getsizeof(newfiles)))
+            print('\nSize of all files: ' + sizeof_fmt(totalsize))
         except StopIteration:
             pass
         finally:
             configuration["_fname"] = configuration["_cwd"]
             del getfiles
-
-        ##Stats
-        print('\nIterated ' + str(x) + ' times. File dictionary is ' + str(len(newfiles)) + ' in length.')
-        print('\nSize of dict in memory is ' + sizeof_fmt(getsizeof(newfiles)))
-        # print('\nSize of all files: ' + sizeof_fmt(totalsize))
     else:
         print(configuration["_cwd"] + " either is not a directory, doesn't exist, or denies access.")
 
     return True
 
-##Save Updates after current scan
+
+# #Save Updates after current scan
 def savelist():
     global configuration
     hfn = hashedfilename(configuration["_fname"])
-    if dictSave(hfn, newfiles):
-        configuration["history"][hfn]=configuration["_fname"]
+    if savedict(hfn, newfiles):
+        configuration["history"][hfn] = configuration["_fname"]
         print('Scan of ' + configuration["_fname"] + ' saved.')
     else:
         print('Error saving ' + configuration["_fname"])
     return True
 
-##Show last scan of current directory, if exists
+
+# #Show last scan of current directory, if exists
 def showlast():
     global oldfiles
     if hashedfilename(configuration["_cwd"]) in configuration["history"]:
         try:
-            oldfiles = dictLoad(hashedfilename(configuration["_cwd"]))
+            oldfiles = loaddict(hashedfilename(configuration["_cwd"]))
             print('Loaded ' + configuration["_cwd"])
             for c in oldfiles:
                 print('|' + c + '        |   ' + oldfiles[c].type + '|      ' + str(oldfiles[c].modified) + '|    ' + str(
@@ -215,68 +226,77 @@ def showlast():
         print("%s has no previous scan data. Returning to menu."%configuration["_cwd"])
         return True
 
-##Sync function goes here
-#def sync(File)
-#If new, upload
-#If existing, overwrite
-#If deleted, delete from S3
 
-##S3 verify function goes here
-#load oldfiles for _cwd
-#for each in oldfiles, check S3
+# #Sync function goes here
+# def sync(File)
+# If new, upload
+# If existing, overwrite
+# If deleted, delete from S3
+
+
+# #S3 verify function goes here
+# load oldfiles for _cwd
+# for each in oldfiles, check S3
 #  update oldfiles
 
-##Handy one-shot for clearing CLI
+
+# #Handy one-shot for clearing CLI
 def clear():
-    if system()=="Windows":
+    if system() == "Windows":
         _=os.system('cls')
     else:
         _=os.system('clear')
 
-##Garbage function for debugging
+
+# #Garbage function for debugging
 def cwd():
     print("Current working directory is " + configuration["_cwd"])
     return True
 
-##Garbage function for debugging
+
+# #Garbage function for debugging
 def printupdates():
     # State: 0=no change, 1=updated, 2=new, 3=deleted
-    state = ['No Change','Updated','New','Deleted']
-    if  len(newfiles)>0:
+    state = ['No Change', 'Updated', 'New', 'Deleted']
+    if len(newfiles) > 0:
         for c in newfiles:
-            if newfiles[c].state >=0:
+            if newfiles[c].state >= 0:
                 print('|' + c + '       |   ' + str(newfiles[c].modified) + '|\t\t' + state[newfiles[c].state])
     else:
         print('No state changes detected or scan not yet done.')
     return True
 
-##Garbage function for debugging
+
+# #Garbage function for debugging
 def showhashed():
     print(hashedfilename(configuration["_cwd"]))
     return True
 
-##Exit
-def exit():
+
+# #Exit
+def done():
     return False
 
-##switchplate; handy way for switch logic progcontrol
+
+# #switchplate; handy way for switch logic progcontrol
 def switchplate(argument):
-    switchplate = {
-        "setdir" : setdir,
-        "cwd" : cwd,
-        "listdirs" : listdirs,
-        "scandir" : scandir,
-        "printupdates" : printupdates,
-        "filecompare" : filecompare,
-        "savelist" : savelist,
-        "showlast" : showlast,
-        "exit" : exit,
-        "showhashed" : showhashed
+    sp = {
+        "setdir": setdir,
+        "cwd": cwd,
+        "listdirs": listdirs,
+        "scandir": scandir,
+        "printupdates": printupdates,
+        "filecompare": filecompare,
+        "savelist": savelist,
+        "showlast": showlast,
+        "done": done,
+        "showhashed": showhashed
     }
-    func = switchplate.get(argument, lambda: True)
+    func = sp.get(argument, lambda: True)
     return func()
 
-##Damenu
+
+# #Damenu
 def menu():
     clear()
     print("Available commands:")
@@ -285,42 +305,42 @@ def menu():
     print('\tscandir\t\t\t\tScan currently selected directory')
     print('\tsavelist\t\t\tSave current scan, if available')
     print('\tshowlast\t\t\tShow last scan and stats for currently selected directory')
-    print('\texit\t\t\t\tExit\n')
-    cwd();
+    print('\tdone\t\t\t\tExit\n')
+    cwd()
     return input('\nType a command above to continue:  ')
 
 
-########  Inits   ###########
+# #######  Inits   ###########
 
 
-######## Go time   ###########
+# ####### Go time   ###########
 
-#if errorno=2 (file not found) then make it with default values; otherwise, panic.
+# if errorno=2 (file not found) then make it with default values; otherwise, panic.
 try:
-    configuration=dictLoad(_configfile)
+    configuration = loaddict(_configfile)
 except IOError as e:
-    if e.errno==2:
+    if e.errno == 2:
         print("No previous config found.  Using default config.")
     else:
         print("Could not load config.  I/O error({0}): {1}".format(e.errno, e.strerror))
-        _=input("Press enter to panic.")
+        _ = input("Press enter to panic.")
         quit(0)
 except:
     print("Unforseen error loading config.")
-    _ =input("Press enter to panic.")
+    _ = input("Press enter to panic.")
     quit(0)
 
-#Show menu until selection is made, execute selection, then save config updates.
-#Exits when a function returns false, e.g. the exit() function.
-b=True
-while b==True:
+# Show menu until selection is made, execute selection, then save config updates.
+# Exits when a function returns false, e.g. the done() function.
+b = True
+while b is True:
     select = menu().lower()
     b = switchplate(select)
     try:
-        dictSave(_configfile, configuration)
+        savedict(_configfile, configuration)
     except:
-        print("Could not save new config.  Check directory permissions on " + str(os.path.abspath(os.getcwd())))
+        print("Could not save config.  Check directory permissions on " + str(os.path.abspath(os.getcwd())))
 
 
-########  Dunzo  ##############
+# #######  Dunzo  ##############
 
